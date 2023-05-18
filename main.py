185ce11c028c83a18c621759baf0edaf88b6280f
Jeffrey Scott <valenpendragon@gmail.com>
2023-05-18 04:58:52 -0400

    Implement basic skeleton of program's booking functionality to find new methods and attributes for Hotel and ReservationConfirmation classes.

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+import pandas as pd
+df = pd.read_csv("./data/hotels.csv")
+
+
 class Hotel:
+    def __init__(self, id: int):
+        pass
+
     def book(self):
+        pass
+
+    def available(self):
         pass
 
 
 class ReservationConfirmation:
+    def __init__(self, customer_name: str, hotel_object: int):
+        pass
+
     def generate(self):
         pass
 
+
+# Creating a skeleton command line main program.
+print(df)
+id = input("Enter the id of the hotel you wish to book: ")
+hotel = Hotel(id)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationConfirmation(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("That hotel is not available right now.")
